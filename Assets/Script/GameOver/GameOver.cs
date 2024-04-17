8d5a1b908e270fafcdebf362404a8e1c53c79360
neptunia558822 <neptunia558822@gmail.com>
2024-04-18 02:41:09 +0700

    เสร็จเเล้วเหลือตกเเต่ง

--- Assets/Script/GameOver/GameOver.cs
+++ Assets/Script/GameOver/GameOver.cs
@@ -1,18 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameOver : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    void OnTriggerEnter2D(Collider2D other)
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        SceneManager.LoadScene("GameOver");
     }
 }
