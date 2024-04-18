9c0a3a6fab55b6b5d4a2d021cea087e23f5d879b
neptunia558822 <neptunia558822@gmail.com>
2024-04-18 22:11:08 +0700

    เสร็จหมดเเล้ว

--- Assets/Script/Win/SceneWin.cs
+++ Assets/Script/Win/SceneWin.cs
@@ -1,10 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class ScaneWin : MonoBehaviour
 {
+    [SerializeField]
+    private TMP_Text scoreText;
+
     public void LoadScene(string sceneName)
     {
         SceneManager.LoadScene(sceneName);
@@ -12,5 +16,15 @@ public class ScaneWin : MonoBehaviour
     public void ExitGame()
     {
         Application.Quit();
+    }
+
+    public void UpdateScore()
+    {
+        scoreText.text = GameManager.nScore.ToString($"You Score : {GameManager.nScore}");
+    }
+
+    void Update()
+    {
+        UpdateScore();
     }
 }
